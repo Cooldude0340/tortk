02073c9bcca39b7ed951dd2412631d1cfdeb9176
Devill Don <53294521+devillD@users.noreply.github.com>
2021-04-30 15:27:04 +0600

    Update ExecVarsSample.py

--- tortoolkit/consts/ExecVarsSample.py
+++ tortoolkit/consts/ExecVarsSample.py
@@ -11,7 +11,7 @@ except:
         BOT_TOKEN = os.environ.get("BOT_TOKEN")
         BASE_URL_OF_BOT = os.environ.get("BASE_URL")
         # ALLOWED USERS [ids of user or supergroup] seperate by commas
-        ALD_USR = [871967943,1291911714,755983475,1282644258,-1001348584170]
+        ALD_USR = os.environ.get([SUDO_CHATS])
         
         # Time to wait before edit message
         EDIT_SLEEP_SECS = 5
