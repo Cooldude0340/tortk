aec23746734cb9ffcc75bfc16e3a116bae329af4
CD-MCV <84226164+Cooldude0340@users.noreply.github.com>
2021-06-03 12:14:41 +0530

    Update ExecVarsSample.py

--- tortoolkit/consts/ExecVarsSample.py
+++ tortoolkit/consts/ExecVarsSample.py
@@ -6,12 +6,12 @@ except:
         # TODO optimize for vps use fully - currently only heroku is focused
         # Set true if its VPS [currently not fully working]
         IS_VPS = False
-        API_HASH = os.environ.get("API_HASH")
-        API_ID = int(os.environ.get("API_ID"))
-        BOT_TOKEN = os.environ.get("BOT_TOKEN")
-        BASE_URL_OF_BOT = os.environ.get("BASE_URL")
+        API_HASH = os.environ.get("fda406cc4f648c303a0fb77255f2a026")
+        API_ID = int(os.environ.get("2712818"))
+        BOT_TOKEN = os.environ.get("1856887661:AAFmbr2Q4bu6TCRMRFyVjIDDIrazmVWQ1mM")
+        BASE_URL_OF_BOT = os.environ.get("tor-tool-kit-tg.heroku.app.com")
         # ALLOWED USERS [ids of user or supergroup] seperate by commas
-        ALD_USR = [871967943,1291911714,755983475,1282644258,-1001348584170]
+        ALD_USR = [1517181772,-1001224781919]
         
         # Time to wait before edit message
         EDIT_SLEEP_SECS = 5
@@ -29,7 +29,7 @@ except:
         REMAINING_STR = "▱"
 
         # DB URI for access
-        DB_URI = os.environ.get("DATABASE_URL")
+        DB_URI = os.environ.get("postgres://arkpvqjxvdfqso:3bf8ceab15e4553d9c7b55f23089de6d04da6454c3ab50703361da912024f776@ec2-54-157-100-65.compute-1.amazonaws.com:5432/das272rbtp2bei")
         
         # The base direcory to which the files will be upload if using RCLONE
         RCLONE_BASE_DIR = "/"
